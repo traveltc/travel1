90a896433740ebad1898825af60625bd0362907a
traveltc <42306171+traveltc@users.noreply.github.com>
2018-08-11 23:25:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.onetravel.com/Package/PackageListing.aspx?tabid=5240");
 die();
 ?>
